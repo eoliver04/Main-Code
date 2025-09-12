cac2ede518504e9bb7b6b56ae0b52eba8c27a60a
eoliver04 <ernestoliver2004@gmail.com>
2025-09-12 10:32:20 -0400

    deno1.2

--- supabase/cliente.ts
+++ supabase/cliente.ts
@@ -1,5 +1,5 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supUrl = Deno.env.get("SUP_URLL") as string;
+const supUrl = Deno.env.get("SUP_URL") as string;
 const supKey = Deno.env.get("key") as string;
 export const supabase = createClient(supUrl, supKey);
